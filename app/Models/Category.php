c6367fdb218ee78518c4ffe98299d26140715e3b
Suman Rana <sumanrana2062@gmail.com>
2023-10-05 17:31:47 +0545

    changed in category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,15 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Category extends Model
 {
     use HasFactory;
 
-    public function product(): HasOne
+    public function product(): HasMany
     {
-        return $this->hasOne(Product::class);
+        return $this->hasMany(Product::class);
     }
 }
 
